d84aa4b62a6a1f42e7404965756367f772b2a890
alvaro <garciajaenalvaro@gmail.com>
2022-09-12 22:26:01 +0200

    add script to install dotfiles

---
+++ install-dotfiles.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt install -y git zsh
+
+config() {
+    git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME "$@"
+}
+
+git clone --bare https://github.com/alvarontwrk/dotfiles.git $HOME/.dotfiles
+mv .bashrc .bashrc.bak
+config checkout
+config config --local status.showUntrackedFiles no
+
+zshpath=$(which zsh)
+sudo sed -i 's@'"$HOME"'.*@'"$HOME"':'"$zshpath"'@g' /etc/passwd
+
+wget -qO- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash
+mv .zshrc.pre-oh-my-zsh .zshrc
+mkdir -p .cache/zsh
+touch .cache/zsh/history
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
