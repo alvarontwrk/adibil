bfb014380a138b234b076bb671162a4004d5ae9b
Alvaro GJ <agarcia@jtsec.es>
2022-09-13 12:15:16 +0200

    keep zshrc

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -15,7 +15,7 @@ config config --local status.showUntrackedFiles no
 zshpath=$(which zsh)
 sudo sed -i 's@'"$HOME"'.*@'"$HOME"':'"$zshpath"'@g' /etc/passwd
 
-wget -qO- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash --keep-zshrc
 mv .zshrc.pre-oh-my-zsh .zshrc
 mkdir -p .cache/zsh
 touch .cache/zsh/history
